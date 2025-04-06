d4bb8910ff3795de605d9446e558ae129d5ed3d3
Marwan Tarek Zaky <marwantarekzaky@gmail.com>
2025-04-07 00:37:24 +0200

    Initial commit

---
+++ Adminstration/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Adminstration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
